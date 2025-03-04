86526d3bf9a44eb20ca17f6aa05a0654c1efa5a5
gui24xr <guillermoxr24@gmail.com>
2025-03-04 15:49:48 -0300

    ss

--- src/app/api/medics/route.ts
+++ src/app/api/medics/route.ts
@@ -2,35 +2,30 @@ import { NextResponse } from "next/server";
 import { MedicsRepository } from "@/app/libs/repositories/medics.repository";
 import { medicSchema } from "@/app/schemas/medic.schema";
 
-const medicsRepository = MedicsRepository.getInstance()
+const medicsRepository = MedicsRepository.getInstance();
 
-
-export async function GET () {
-     try{
-          const medics = await medicsRepository.getMedics()
+export async function GET() {
+  try {
+    const medics = await medicsRepository.getMedics();
+    return NextResponse.json(medics);
+  } catch (error: any) {
     return NextResponse.json(
-         medics
-    )
-     }catch(error:any){
-          return NextResponse.json(
-               { message: 'Error al intentar obtener medicos', error: error.message },
-               { status: 500 }
-             );
-     }
-    
+      { message: "Error al intentar obtener medicos", error: error.message },
+      { status: 500 }
+    );
+  }
 }
 
-export async function POST (request: Request){
-     try{
-          const data = await request.json()
-          medicSchema.parse(data)
-          const newMedic = await medicsRepository.createMedic(data)
-          return NextResponse.json(newMedic)
-     }catch(error:any){
-          return NextResponse.json(
-               { message: 'Error al intentar crear medico', error: error.message },
-               { status: 500 }
-             );
-     }
-
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    medicSchema.parse(data);
+    const newMedic = await medicsRepository.createMedic(data);
+    return NextResponse.json(newMedic);
+  } catch (error: any) {
+    return NextResponse.json(
+      { message: "Error al intentar crear medico", error: error.message },
+      { status: 500 }
+    );
+  }
 }
